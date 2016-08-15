5f0751eff0fd77d2fb04fab6f3be93f44a0900ab
hanmiton <hanmilton_12@outlook.com>
2016-08-14 23:34:05 -0500

    añadienod selectore eln jquery

---
+++ jquery.js
@@ -0,0 +1,46 @@
+$ //es lo mismo que llmar a jqueyr
+window.onload = function(){ //funcion que se ejecuta cuando se carag todo el DOM
+//desventaja que puede demorarse mucho tiempo
+}
+//jquery es mucho mas conveniente usar la siguierne opcion a onload
+$(document).ready(funcion(){//funcion que carga cuano el docuemtneo se a cargado mejor q onload
+	alert('ready')// funcion que manda una alerta al browser
+})
+$().ready(function(){})//lo mismo que hacer con documetn
+$(function(){})//lo mismo lo ejecuta cuando el documetno esta ready
+//cuadno usamos con prototype
+$.noConflict();
+jQuery(document).ready(function($){
+	$ //jQuery
+})
+$ //prototype fuera de jquery
+///////
+//Selectores jquery
+document.getElementById('app-header')//selecionar element ocn vanilla javascipt
+$('#app-header h1')//selecciona el h1 dentro de elemento id app-header
+$('h1') //selecciona toods los h1
+$('h1.title') //h1 con la clase title
+$('h1 + h2') //todos los h2 hermanos de h1
+/////seleccionar dentor de un contexto
+var header = $('header'); //seleccion el elmento header
+var title = $('h1', header); //seleccion h1 dentro de header(contexto) funcin('elemento', ctx)
+//se devuelve difernte con javascript q con jquery
+//javascript devuele un obejto del DOM
+//JQUERY devuelve un JqueryObject
+var header = $('header'); //seleccion el elmento header
+var title = $('h1', header[0]); //seleccionar el primer  h1 dentro de header[0](contexto) funcin('elemento', ctx)
+//pasando elemento de DOM a jquery
+var header = document.getElementById('app-header');
+var $header = $(header); //pasamdno elmento del DOMO A jquery object
+//selectionana multiples
+var headings = $('h1, h2');//selecioonand tanto h1 o h2
+//enviando array
+var sleccion = $([documetn, header]);
+$(':input')//pseudoselectores (seleccionando input dentro de form <input>, <textarea>)
+$(':selected')//seleccion ar todo los elemento suq ehaya sido selleccionados
+$(':enabled') //selecciona todo los elmentos habilitados
+$(':disabled') //elementos desabilitados
+$(':text') //los de tipo text
+$(':checkbox')//seleccionar todo los raido butoton
+//JqueryObject
+//
--- readme.md
+++ readme.md
@@ -1,1 +1,41 @@
-Pimer commit jquery
+Jquery (facilita ahcer los request,premite facilitar los eventos, manejo de errores)
+Jquery-lodash-underscore (son librerias competidoras para jquery)
+Jquery no cubre rutas (existe plugin pero exiten librerais que lo hace mejo)
+Javascript (permite agregar ocmporatameinto a una pagian web)
+npm gestor de paquetes (reutilizcino de codigo qu ya esta hecho)
+DOM(documetn object model)
+Xamp(para alzar un servidor en local)
+
+----------JQUERY---------------
+renderizaicn
+eventos
+enrutamiento 
+llamadas 
+(paso por parte del cliente)
+autenticidad
+(backend)
+--------APi REst-----
+rest (protocolo pra la comunicacion servidor cliente)
+Json(javascript only notacion clave-valor)
+http(s) Request
+	get(obteniene los datos de uan api)
+	post(subir datos la servidor)
+	put
+	delete
+Same-Origin-Policy(solo se peuden hacer peticiones desde el mismo dominio de lapagina)
+JsonP(solo con metodos get json con padding)
+Cors(Cross-Origin-Resource-Sharing)( si el servidor lo da es que los podemos utilizar)
+API REST(End Points)
+DOM(arbol de jerarquia que comopone nuestro ducumento)
+Jqueyr pra manipular el dom
+Jquery
+	- compatibilidad con browser
+	- convenencia (alternativa javascript vanilla)
+	- facilidad para hacer reqeuts tipo ajax
+Merjo usar cdn apra cargar jqeury
+
+Probando si vale jquery
+	presionams f12
+	jQuery
+		(si apracera la funcion(a,b){return ......})(jquery si se encuentra definido)
+		$ es lo mismo que jquery
